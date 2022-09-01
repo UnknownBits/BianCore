4b86b6b2882e8c1e9e7cc9016f8f079ff3509a91
Admin <Admin@DESKTOP-O4RGU2O>
2022-09-01 17:16:51 +0800

    添加项目文件。

---
+++ Tools/Downloads.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bian.Tools
+{
+    internal class Downloads
+    {
+        /// <summary>
+        /// 下载文件(单线程)
+        /// </summary>
+        /// <param name="url">下载链接</param>
+        /// <param name="save">保存地址</param>
+        /// <returns></returns>
+        public static void Plan1(string url, string save)
+        {
+            using (var web = new WebClient())
+            {
+                web.DownloadFile(url, save);
+            }
+        }
+        private static List<string> DList = new List<string>();
+        private static List<string> DPath = new List<string>();
+        private static List<string> DName = new List<string>();
+        public static void AddDList(string link, string path, string name = null)
+        {
+            DPath.Add(path);
+            DList.Add(link);
+            DName.Add(name);
+        }
+        public static async Task Plan2()
+        {
+            for (int i = 0; i < DList.Count; i++)
+            {
+                await DList[i].DownloadFileAsync(DPath[i], DName[i]);
+            }
+
+        }
+    }
+}
---
+++ Tools/Interface1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bian.Tools
+{
+    internal interface Interface1
+    {
+    }
+}
