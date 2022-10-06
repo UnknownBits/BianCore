bf17f8cb0ff8e32f27380d2e1c76313955445a4e
HuaJiKing402 <3550514081@qq.com>
2022-10-06 18:31:30 +0800

    Fix

--- Core/Config.cs
+++ Core/Config.cs
@@ -38,21 +38,18 @@ namespace BianCore.Core
         /// 获取该应用程序工作目录。
         /// </summary>
         /// <returns>工作目录。</returns>
-        public static string WorkPath()
-        {
-            return RootPath() + Project_Name + '/';
-        }
-        public static string ConfigPath { set; get; } = WorkPath() + "Config.bian";
-        public static string Background = WorkPath() + "Backgroud/";
+        public static string WorkPath = $"{RootPath()}{Project_Name}/";
+        public static string ConfigPath { set; get; } = WorkPath + "Config.bian";
+        public static string Background = WorkPath + "Backgroud/";
         public static string BackgroundFile = Background + "Background.png";
-        public static string Music = WorkPath() + "Music/";
+        public static string Music = WorkPath + "Music/";
         public static JObject BingBackGroud_Data = Json.Str_to_Json(Network.HttpGet("https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN"));
-        public static string InfoFile { get; } = $"{WorkPath()}Info/{Tools.SystemTools.GetTimestamp("hh:mm:ss")}";
+        public static string InfoFile { get; } = $"{WorkPath}Info/{SystemTools.GetTimestamp("hh:mm:ss")}";
 
 
         internal static class Hiper
         {
-            public static string WorkPath = WorkPath() + "Hiper/";
+            public static string WorkPath = RootPath() + "Hiper/";
             public const string Download_URL = "https://gitcode.net/to/hiper/-/raw/master/";
             public const string HashMap_URL = Download_URL + "packages.sha1";
             public static string InfoFile = WorkPath + $"Info/{DateTime.Now.Date.ToString("yyyy-MM-dd")}.Info";
