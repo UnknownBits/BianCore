a419c02ca397acdf00e4a517d5e405cac1d7dd9b
HuaJiKing <3485566458@qq.com>
2022-10-16 11:16:17 +0800

    Fix

--- Tools/API/Bing.cs
+++ Tools/API/Bing.cs
@@ -1,5 +1,4 @@
-﻿using Flurl;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Text;
--- Tools/Downloads.cs
+++ Tools/Downloads.cs
@@ -5,8 +5,6 @@ using System.Net;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using Flurl;
-using Flurl.Http;
 
 namespace BianCore.Tools
 {
