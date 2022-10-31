72b746a98e896aea4d57312f16dc35ae1b92df84
WinExp <1450422389@qq.com>
2022-10-31 22:30:42 +0800

    修复 Bug #1

--- API/Minecraft.cs
+++ API/Minecraft.cs
@@ -14,7 +14,7 @@ namespace BianCore.API
         public static async Task<AuthenticateMinecraftResponse> AuthenticateMinecraftRequest(string userhash, string xsts_token)
         {
             string url = "https://api.minecraftservices.com/authentication/login_with_xbox";
-            string param = "{\"identityToken\":\"XBL3.0 x=" + $"{userhash};{xsts_token}" + "\"}";
+            string param = $"{{\"identityToken\":\"XBL3.0 x={userhash};{xsts_token}\"}}";
             using var content = new StringContent(param);
             string responseStr = await Network.HttpPost(url, content);
             var response = JsonConvert.DeserializeObject<AuthenticateMinecraftResponse>(responseStr);
