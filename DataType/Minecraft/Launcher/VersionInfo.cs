d6547f98b6b940fbcf2d983eb5fd132cc81ffa3b
WinExp <1450422389@qq.com>
2022-11-20 16:13:53 +0800

    + 添加 VersionInfo.cs

---
+++ DataType/Minecraft/Launcher/VersionInfo.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace BianCore.DataType.Minecraft.Launcher
+{
+    public struct VersionInfo
+    {
+        public struct ArgumentsStruct
+        {
+            [JsonIgnore]
+            public string[] Game { get; set; }
+
+            [JsonIgnore]
+            public string[] JVM { get; set; }
+
+            private JToken game;
+
+            private JToken jvm;
+
+            [OnDeserialized]
+            private void OnDeserialized()
+            {
+                List<string> strings = new List<string>();
+                foreach (var item in game)
+                {
+                    if (item.Type == JTokenType.String)
+                    {
+                        strings.Add(item.ToString());
+                    }
+                    else
+                    {
+                        bool isAllow = (string)item["$.rules[0].action"] == "allow";
+                    }
+                }
+            }
+        }
+    }
+}
--- Module/Minecraft/Launcher.cs
+++ Module/Minecraft/Launcher.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace BianCore.Module.Minecraft
 {
-    internal class Launcher
+    public class Launcher
     {
+        public string VersionsPath { get; set; }
+
+        /// <summary>
+        /// Launcher 类的构造方法
+        /// </summary>
+        /// <param name="versionsPath">versions 文件夹的路径 E.g. D:\Minecraft\.minecraft\versions</param>
+        public Launcher(string versionsPath)
+        {
+            VersionsPath = versionsPath;
+        }
+
+
     }
 }
