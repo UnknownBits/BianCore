34d6ead61675340dd16eeb888364d22bf897c8a0
WinExp <1450422389@qq.com>
2022-11-25 22:16:03 +0800

    + 部分 Minecraft 版本 JSON 序列化

---
+++ DataType/Minecraft/Launcher/AssetIndexStruct.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BianCore.DataType.Minecraft.Launcher
+{
+    public struct AssetIndexStruct
+    {
+        [JsonProperty("id")]
+        public string ID { get; set; }
+
+        [JsonProperty("sha1")]
+        public string SHA1 { get; set; }
+
+        [JsonProperty("size")]
+        public long Size { get; set; }
+
+        [JsonProperty("totalSize")]
+        public long TotalSize { get; set; }
+
+        [JsonProperty("url")]
+        public string URL { get; set; }
+    }
+}
