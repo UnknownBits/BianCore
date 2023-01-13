8f44f111c9c4de4c6a44b16e2461808f9e7902bd
HuaJiKing402 <3550514081@qq.com>
2023-01-13 20:44:49 +0800

    *临时提交#1

--- Modules/BianNetwork/Tcp.cs
+++ Modules/BianNetwork/Tcp.cs
@@ -1,6 +1,25 @@
-﻿namespace BianCore.Modules.BianNetwork
+﻿using System.Net.Sockets;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+namespace BianCore.Modules.BianNetwork
 {
     public class Tcp
     {
+        public string Listen(Int32 port){
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            TcpListener listener = new TcpListener(localAddr,port);
+            try
+            {
+                listener.Start();
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+            return null;
+        }
     }
 }
--- Modules/BianNetwork/WebSocket.cs
+++ Modules/BianNetwork/WebSocket.cs
@@ -4,7 +4,6 @@
     {
         public dynamic CreateClient()
         {
-
             return null;
         }
         public dynamic CreateServer()
